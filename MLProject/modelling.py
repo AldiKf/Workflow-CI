0060752aa35a1de8b558933f221dcd01013b7ddc
AldiKf <143716494+AldiKf@users.noreply.github.com>
2025-12-24 15:46:19 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,6 +1,5 @@
 import os
 os.environ["MLFLOW_SKLEARN_DISABLE_TORCH"] = "true"
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,29 +8,20 @@ import mlflow
 import mlflow.sklearn
 
 df = pd.read_csv("student_score_preprocessing.csv")
-
 X = df.drop(columns=["exam_score"])
 y = df["exam_score"]
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.set_experiment("Student Exam Score - CI")
-
-with mlflow.start_run():
+with mlflow.start_run():  
     mlflow.sklearn.autolog()
-
     model = LinearRegression()
     model.fit(X_train, y_train)
-
     y_pred = model.predict(X_test)
-
+    
     mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
         registered_model_name="StudentScoreModel"
     )
-
-
-
